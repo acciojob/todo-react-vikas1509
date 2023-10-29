3c20f687acc1a055968b5f2dba612a5788121a53
vikas1509 <vjchoudhary1234@gmail.com>
2023-10-29 21:08:08 +0530

    submitting code-Sun Oct 29 2023 21:08:08 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo, settodo] = useState([]);
+  const [text, settext] = useState('');
+
+  function addToDo() {
+    if (text.trim() !== '') {
+      settodo([...todo, text]);
+      settext("");
+    }
+  }
+
+  function DeleteToDo(index) {
+    const newSetToDo = todo.filter((_, i) => i !== index);
+    settodo(newSetToDo);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do App</h1>
+      <input type="text" value={text} onChange={(e) => settext(e.target.value)} placeholder="Enter a new Task" />
+      <button onClick={addToDo}>Add</button>
+      <ul>
+        {todo.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => DeleteToDo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App;
+
